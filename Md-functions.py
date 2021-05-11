f305bdf48835bdfb1395d643f5adbcdb318d8d1f
Mohammed Aamir <mdaamirsiddiqui9857@gmail.com>
2021-05-11 10:18:09 +0530

    Update Md-functions.py
    Added one more function and modified takeinput() function

--- Md-functions.py
+++ Md-functions.py
@@ -1,12 +1,32 @@
-def takeinput(typeofinput,text,exceptext):
-    #usage
-    # usr_input = takeinput('int', 'What is your age: ', 'wrong age entered!')
-    # print(usr_input)
+def yesorno(self,yesact,noact):
+    opt = [1,0]
+    while True:
+        try:
+            output = int(input(f"Type '1' for {yesact} or '0' for {noact}: "))
+            if output not in opt:
+                print("You can only enter '1' or '0'" )
+                continue
+            else: 
+                break
+        except Exception as e:
+            print("Enter a right option!")
+            continue
+    return output
+def takeinput(self,typeofinput,text,exceptext):
     if typeofinput == 'int':
         while True:
             try:
                 output = int(input(text))
-                return output
                 break
             except:
                 print(exceptext)
+        return output
+    elif typeofinput == 'string':
+        while True:
+            try:
+                output = input(text)
+                break
+            except:
+                print(exceptext)
+        return output
+
